f116cd01a5bcf2665e7787c6c48a17a416f88636
Daniel Pinheiro Ramos <daniel.ramos5291@gmail.com>
2022-01-31 18:35:04 -0300

    Mensagem erro 400 da idade atual do Usuario Menor de 18

--- src/main/java/br/org/generation/lojagames/service/UsuarioService.java
+++ src/main/java/br/org/generation/lojagames/service/UsuarioService.java
@@ -27,9 +27,14 @@ public class UsuarioService {
 		if (usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent())
 			return Optional.empty();
 			
-		if (calcularIdade(usuario.getDataNascimento()) < 18)
+		int idade = calcularIdade(usuario.getDataNascimento());
+		int anos = 18 - idade;
+		
+		if ( idade < 18)
 			throw new ResponseStatusException(
-					HttpStatus.BAD_REQUEST, "Usuário é menor que 18 anos!", null);
+					HttpStatus.BAD_REQUEST, "Usuário é menor que 18 anos! Sua idade é de " 
+			        + idade + " anos! aguarde " + anos + " anos e Tente Novamente!;)", null);
+		
 		
 		usuario.setSenha(criptografarSenha(usuario.getSenha()));
 
